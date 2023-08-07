4519f29815ca7677c83a551a3547a161f2095c74
zidanindratama <zidanindratama31@gmail.com>
2023-08-08 05:41:16 +0700

    stipe

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
+// publishable  : pk_test_51NcNYeGGXjd0KQoxICXOWfhRGSAzGkfqqenjaZzsgEEHcosX0UI1kotaYN3CtQ6fqRrxbGMPiofsYCC2kzlDgCYa009opV2IaV
+// secret       : sk_test_51NcNYeGGXjd0KQoxBp5aanLRpUFkzi1g7EFwJ4ljjqRfMb7kuo1Nzb4WLjZxzF2TaFJ4RT3UUXTtk8ZXlphxnLxp003EnIPlvf
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import Stripe from "stripe";
+const stripe = new Stripe(
+  "sk_test_51NcNYeGGXjd0KQoxBp5aanLRpUFkzi1g7EFwJ4ljjqRfMb7kuo1Nzb4WLjZxzF2TaFJ4RT3UUXTtk8ZXlphxnLxp003EnIPlvf"
+);
 
 import * as middleware from "./utils/middleware.js";
 import helloRoute from "./routes/helloRouter.js";
@@ -22,6 +28,31 @@ app.get("/", (req, res) => {
 });
 
 app.use("/hello", helloRoute);
+
+app.post("/checkout", async (req, res) => {
+  console.log(req.body);
+  const items = req.body.items;
+  let lineItems = [];
+  items.forEach((item) => {
+    lineItems.push({
+      price: item.id,
+      quantity: item.quantity,
+    });
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: "payment",
+    success_url: "https://mition-website-klinik24-1ia09.netlify.app/success",
+    cancel_url: "https://mition-website-klinik24-1ia09.netlify.app/",
+  });
+
+  res.send(
+    JSON.stringify({
+      url: session.url,
+    })
+  );
+});
 
 // custom middleware
 app.use(middleware.unknownEndpoint);
